02329b353a6ca01ce5252eb0fd7c75ee5b76a464
mpadge <mark.padgham@email.com>
2016-11-04 11:18:06 +0100

    update GDAL install

--- install-gis.sh
+++ install-gis.sh
@@ -15,7 +15,21 @@ echo deb https://josm.openstreetmap.de/apt alldist universe | sudo tee /etc/apt/
 wget -q https://josm.openstreetmap.de/josm-apt.key -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt install josm
-sudo apt-get install -y gdal-bin libgdal-dev libgdal1-dev libproj-dev libgeos++-dev
+#sudo apt-get install -y gdal-bin libgdal-dev libgdal1-dev libproj-dev libgeos++-dev
+sudo apt-get install -y libproj-dev libgeos++-dev
+GDAL_URL="http://download.osgeo.org/gdal/CURRENT/"
+wget -O temp.html $GDAL_URL
+GDAL_VERSION=`cat temp.html | grep -o -P '(?<=gdal-).*(?=.tar.gz\")'`
+GDAL_FILE="gdal-"$GDAL_VERSION".tar.gz"
+rm temp.html
+wget $GDAL_URL$GDAL_FILE
+tar zxf $GDAL_FILE
+cd "gdal-"$GDAL_VERSION
+./configure
+make
+sudo make install
+cd ..
+rm -rf "gdal-"$GDAL_VERSION $GDAL_FILE
 
 # install R/RStudio - see
 # http://stackoverflow.com/questions/29667330
