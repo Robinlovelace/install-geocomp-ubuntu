615118981e978d1f8ae9ecffa97ac50629c9b055
robinlovelace <rob00x@gmail.com>
2016-11-05 10:56:26 +0000

    Revert to apt-get gdal install

--- install-gis.sh
+++ install-gis.sh
@@ -17,19 +17,7 @@ sudo apt-get update
 sudo apt install josm
 #sudo apt-get install -y gdal-bin libgdal-dev libgdal1-dev libproj-dev libgeos++-dev
 sudo apt-get install -y libproj-dev libgeos++-dev
-GDAL_URL="http://download.osgeo.org/gdal/CURRENT/"
-wget -O temp.html $GDAL_URL
-GDAL_VERSION=`cat temp.html | grep -o -P '(?<=gdal-).*(?=.tar.gz\")'`
-GDAL_FILE="gdal-"$GDAL_VERSION".tar.gz"
-rm temp.html
-wget $GDAL_URL$GDAL_FILE
-tar zxf $GDAL_FILE
-cd "gdal-"$GDAL_VERSION
-./configure
-make
-sudo make install
-cd ..
-rm -rf "gdal-"$GDAL_VERSION $GDAL_FILE
+sudo apt-get install -y gdal-bin libgdal-dev libgdal1-dev 
 
 # install R/RStudio - see
 # http://stackoverflow.com/questions/29667330
